4a7aa2d580046db2578a50165ac41fe25cdaa9d7
Ebrar <ebrarkbenli@gmail.com>
2026-04-29 22:35:46 +0300

    Change folder structure

--- Backend/FinDataTracker.Api/DTOs/StockResponseDto.cs
+++
@@ -1,9 +0,0 @@
-namespace FinDataTracker.Api.DTOs;
-
-public class StockResponseDto
-{
-    public int Id { get; set; }
-    public string Symbol { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-    public DateTime LastUpdated { get; set; }
-}
--- Backend/FinDataTracker.Api/Models/Stock.cs
+++
@@ -1,9 +0,0 @@
-namespace FinDataTracker.Api.Models;
-
-public class Stock
-{
-    public int Id { get; set;}
-    public string Symbol { get; set;} = string.Empty;
-    public decimal Price { get; set;}
-    public DateTime LastUpdated { get; set;}
-}
